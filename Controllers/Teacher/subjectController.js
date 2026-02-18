c6540b630f63336eaf8ff515c1893d617299bf9c
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-18 15:55:04 +0500

    add new fields

--- Controllers/Teacher/subjectController.js
+++ Controllers/Teacher/subjectController.js
@@ -2,6 +2,19 @@ import Subject from '../../Models/subjectModel.js';
 import Attendance from '../../Models/attendanceModel.js';
 import mongoose from 'mongoose';
 
+const subjectColors = [
+  'bg-blue-500',
+  'bg-green-500',
+  'bg-purple-500',
+  'bg-orange-500',
+  'bg-red-500',
+  'bg-indigo-500',
+  'bg-pink-500',
+  'bg-teal-500',
+  'bg-cyan-500',
+  'bg-amber-500'
+];
+
 export const createSubject = async (req, res) => {
   try {
     const {
