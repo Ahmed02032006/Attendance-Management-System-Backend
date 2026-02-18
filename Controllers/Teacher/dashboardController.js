b85c97808c57e9d9dc9ec74f2fe766702f96ed28
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-18 17:06:13 +0500

    solving issues

--- Controllers/Teacher/dashboardController.js
+++ Controllers/Teacher/dashboardController.js
@@ -52,7 +52,7 @@ export const getSubjectsByUser = async (req, res) => {
           title: subject.subjectTitle,
           code: subject.subjectCode,
           students: studentCount.length,
-          createdAt: studentCount.createdDate,
+          createdAt: subject.createdDate,
           color: subjectColors[index % subjectColors.length] // Cycle through colors
         };
       })
